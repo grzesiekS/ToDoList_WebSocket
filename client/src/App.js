dba117f50d470ace779a28659ce494671bf61d99
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-09-13 18:21:36 +0200

    Fix bug in removeTask method in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -15,8 +15,10 @@ class App extends React.Component {
     this.socket.on('updateData', tasksServer => this.setState({ tasks: tasksServer }));
   }
 
-  removeTask(taskIndex) {
-    this.socket.emit('removeTask', taskIndex);
+  removeTask(taskIndex, event) {
+    if(event !== undefined) {
+      this.socket.emit('removeTask', taskIndex);
+    }
     this.setState({tasks: this.state.tasks.filter(task => this.state.tasks.indexOf(task) !== taskIndex)});
   }
 
@@ -49,7 +51,7 @@ class App extends React.Component {
         <ul className="tasks-section__list" id="tasks-list">
           {this.state.tasks.map(task => (
             <li className="task" key={this.state.tasks.indexOf(task)}>{task} 
-              <button className="btn btn--red" onClick={() => this.removeTask(this.state.tasks.indexOf(task)) }>Remove</button>
+              <button className="btn btn--red" onClick={event => this.removeTask(this.state.tasks.indexOf(task),event) }>Remove</button>
             </li>
           ))}
         </ul>
